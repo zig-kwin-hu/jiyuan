be324b9acf78efee2262fb0f06d77e8b74ecfdc3
fzh14 <fzh14@mails.tsinghua.edu.cn>
2016-11-29 19:49:39 +0800

    冒险检测，旁路+跳转指令

--- Forward.vhd
+++ Forward.vhd
@@ -8,8 +8,15 @@ entity Forward is
            	MEMWBrd : in  STD_LOGIC_VECTOR (3 downto 0);--MW.RegDst
            	IDEXrx : in  STD_LOGIC_VECTOR (3 downto 0);--DE.R1
            	IDEXry : in  STD_LOGIC_VECTOR (3 downto 0);--DE.R2
-           	ForwardA : out  STD_LOGIC_VECTOR (1 downto 0);
-           	ForwardB : out  STD_LOGIC_VECTOR (1 downto 0));
+           	ForwardA : out STD_LOGIC_VECTOR(1 downto 0);
+           	ForwardB : out STD_LOGIC_VECTOR(1 downto 0);
+
+           	ForwardC : out STD_LOGIC_VECTOR(1 downto 0);--00-Reg,01-Mem.Re,10-WB.ToReg
+           	IFIDCommand: in STD_LOGIC_VECTOR(15 downto 0);
+           	IFIDR1 : in STD_LOGIC_VECTOR(3 downto 0);
+           	MEMWBMemRead : in STD_LOGIC;
+            EXMEMMemRead : in STD_LOGIC
+           	);
 end Forward;
 
 architecture Behavioral of Forward is
@@ -33,6 +40,17 @@ begin
 	else
 		ForwardB<="00";
 	end if;
+
+  if (IFIDCommand(15 downto 11)="00010" or IFIDCommand(15 downto 11)="00100" or IFIDCommand(15 downto 8)="01100000" or (IFIDCommand(15 downto 11)="11101" and IFIDCommand(7 downto 0)="00000000")) then 
+    if (EXMEMRegWrite='1' and EXMEMMemRead='0' and EXMEMrd=IFIDR1)
+      ForwardC<="01"
+    else
+      ForwardC<="00"
+    end if;
+  else
+      ForwardC<="00";
+  end if;
+
 end process;
 
 end Behavioral;
--- datahazard.vhd
+++ datahazard.vhd
@@ -2,12 +2,15 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity datahazard is
-    Port ( 	IDEXMemRead : in STD_LOGIC;
-           	IDEXRegDst : in STD_LOGIC_VECTOR(3 downto 0);
-           	IFIDr1 : in  STD_LOGIC_VECTOR (3 downto 0);--FD.R1
-           	IFIDr2 : in  STD_LOGIC_VECTOR (3 downto 0);--FD.R2
-           	isBubble : out  STD_LOGIC
-           	);
+    Port (  IDEXMemRead : in STD_LOGIC;
+            IDEXRegDst : in STD_LOGIC_VECTOR(3 downto 0);
+            IFIDr1 : in  STD_LOGIC_VECTOR (3 downto 0);--FD.R1
+            IFIDr2 : in  STD_LOGIC_VECTOR (3 downto 0);--FD.R2
+            IFIDCommand: in STD_LOGIC_VECTOR(15 downto 0);
+            EXMEMMEMRead : in STD_LOGIC;
+            EXMEMRegDst : in STD_LOGIC_VECTOR(3 downto 0);
+            isBubble : out  STD_LOGIC
+            );
 end datahazard;
 
 architecture Behavioral of datahazard is
@@ -15,12 +18,22 @@ architecture Behavioral of datahazard is
 begin
 
 process 
-begin 
-	if (IDEXMemRead='1' and (IFIDr1=IDEXRegDst or IFIDr2=IDEXRegDst)) then
-		isBubble = '1';
-	else
-		isBubble = '0';
-	end if;
+begin
+  if (IFIDCommand(15 downto 11)="00010" or IFIDCommand(15 downto 11)="00100" or IFIDCommand(15 downto 8)="01100000" or (IFIDCommand(15 downto 11)="11101" and IFIDCommand(7 downto 0)="00000000")) then 
+    if (IDEXRegDst=IFIDr1) then
+      isBubble = '1';
+    elsif (EXMEMMEMRead='1' and EXMEMRegDst=IFIDr1) then
+      isBubble = '1';
+    else
+      is isBubble = '0';
+    end if;  
+  else 
+   if (IDEXMemRead='1' and (IFIDr1=IDEXRegDst or IFIDr2=IDEXRegDst)) then
+    isBubble = '1';
+   else
+    isBubble = '0';
+   end if;
+  end if;
 end process;
 
 end Behavioral;
